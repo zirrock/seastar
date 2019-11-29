482c7dd8ccc48bbe31ecceb658cb34d2015b3125
Piotr Grabowski <avelanarius@gmail.com>
2019-11-29 16:46:39 +0100

    kafka: Cleanup of Kafka protocol parsing tests.

--- tests/unit/kafka_protocol_test.cc
+++ tests/unit/kafka_protocol_test.cc
@@ -33,111 +33,69 @@
 
 using namespace seastar;
 
+template <typename KafkaType>
+void test_deserialize_serialize(std::vector<unsigned char> data,
+        KafkaType &kafka_value, int16_t api_version) {
+    boost::iostreams::stream<boost::iostreams::array_source> input_stream(reinterpret_cast<char *>(data.data()),
+            data.size());
+
+    kafka_value.deserialize(input_stream, api_version);
+
+    std::vector<unsigned char> output(data.size());
+    boost::iostreams::stream<boost::iostreams::array_sink> output_stream(reinterpret_cast<char *>(output.data()),
+                                                                          output.size());
+    kafka_value.serialize(output_stream, api_version);
+
+    BOOST_REQUIRE(!output_stream.bad());
+
+    BOOST_TEST(output == data, boost::test_tools::per_element());
+}
+
+template <typename KafkaType>
+void test_deserialize_throw(std::vector<unsigned char> data,
+                                KafkaType &kafka_value, int16_t api_version) {
+    boost::iostreams::stream<boost::iostreams::array_source> input_stream(reinterpret_cast<char *>(data.data()),
+                                                                          data.size());
+
+    BOOST_REQUIRE_THROW(kafka_value.deserialize(input_stream, api_version), kafka::parsing_exception);
+}
+
 BOOST_AUTO_TEST_CASE(kafka_primitives_number_test) {
     kafka::kafka_number_t<uint32_t> number(15);
     BOOST_REQUIRE_EQUAL(*number, 15);
 
-    std::array<char, 4> data{0x12, 0x34, 0x56, 0x78};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(data.begin(), data.end());
-
-    number.deserialize(input_stream, 0);
+    test_deserialize_serialize({0x12, 0x34, 0x56, 0x78}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, 0x12345678);
 
-    std::array<char, 4> output{};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream(output.begin(), output.end());
-    number.serialize(output_stream, 0);
-    BOOST_REQUIRE(!output_stream.bad());
-
-    BOOST_TEST(output == data, boost::test_tools::per_element());
-
-    std::array<char, 2> too_short_data{0x17, 0x27};
-    boost::iostreams::stream<boost::iostreams::array_source> short_input_stream(too_short_data.begin(),
-                                                                                too_short_data.end());
-
-    BOOST_REQUIRE_THROW(number.deserialize(short_input_stream, 0), kafka::parsing_exception);
-
+    test_deserialize_throw({0x17, 0x27}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, 0x12345678);
 }
 
 BOOST_AUTO_TEST_CASE(kafka_primitives_varint_test) {
     kafka::kafka_varint_t number(155);
 
-    std::array<char, 1> data1{0x00};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream1(data1.begin(), data1.end());
-
-    number.deserialize(input_stream1, 0);
+    test_deserialize_serialize({0x00}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, 0);
 
-    std::array<char, 1> output_data1{0x05};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream1(output_data1.begin(), output_data1.end());
-    number.serialize(output_stream1, 0);
-    BOOST_REQUIRE(!output_stream1.bad());
-
-    BOOST_TEST(output_data1 == data1, boost::test_tools::per_element());
-
-    std::array<char, 1> data2{0x08};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream2(data2.begin(), data2.end());
-
-    number.deserialize(input_stream2, 0);
+    test_deserialize_serialize({0x08}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, 4);
 
-    std::array<char, 1> data3{0x07};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream3(data3.begin(), data3.end());
-
-    number.deserialize(input_stream3, 0);
+    test_deserialize_serialize({0x07}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, -4);
 
-    std::array<char, 2> data4{static_cast<char>(0xAC), 0x02};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream4(data4.begin(), data4.end());
-
-    number.deserialize(input_stream4, 0);
+    test_deserialize_serialize({0xAC, 0x02}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, 150);
 
-    std::array<char, 2> output_data4{0x05, 0x05};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream4(output_data4.begin(), output_data4.end());
-    number.serialize(output_stream4, 0);
-    BOOST_REQUIRE(!output_stream4.bad());
-
-    BOOST_TEST(output_data4 == data4, boost::test_tools::per_element());
-
-    std::array<char, 2> data5{static_cast<char>(0xAB), 0x02};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream5(data5.begin(), data5.end());
-
-    number.deserialize(input_stream5, 0);
+    test_deserialize_serialize({0xAB, 0x02}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, -150);
 
-    std::array<char, 2> output_data5{0x05, 0x05};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream5(output_data5.begin(), output_data5.end());
-    number.serialize(output_stream5, 0);
-    BOOST_REQUIRE(!output_stream5.bad());
-
-    BOOST_TEST(output_data5 == data5, boost::test_tools::per_element());
-
-    std::array<char, 1> data_short{static_cast<char>(0xAC)};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream_short(data_short.begin(), data_short.end());
-
-    BOOST_REQUIRE_THROW(number.deserialize(input_stream_short, 0), kafka::parsing_exception);
+    test_deserialize_throw({0xAC}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, -150);
 
-    std::array<char, 5> data_big{static_cast<char>(0xFF), static_cast<char>(0xFF),
-                                 static_cast<char>(0xFF), static_cast<char>(0xFF), 0xF};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream_big(data_big.begin(), data_big.end());
-
-    number.deserialize(input_stream_big, 0);
+    test_deserialize_serialize({0xFF, 0xFF, 0xFF, 0xFF, 0xF}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, -2147483648);
 
-    std::array<char, 5> output_data_big{};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream_big(output_data_big.begin(), output_data_big.end());
-    number.serialize(output_stream_big, 0);
-    BOOST_REQUIRE(!output_stream_big.bad());
-
-    BOOST_TEST(output_data_big == data_big, boost::test_tools::per_element());
-
-    std::array<char, 5> data_too_big{static_cast<char>(0xFF), static_cast<char>(0xFF),
-                                 static_cast<char>(0xFF), static_cast<char>(0xFF), 0x1F};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream_too_big(data_too_big.begin(), data_too_big.end());
-
-    BOOST_REQUIRE_THROW(number.deserialize(input_stream_too_big, 0), kafka::parsing_exception);
+    test_deserialize_throw({0xFF, 0xFF, 0xFF, 0xFF, 0x1F}, number, 0);
     BOOST_REQUIRE_EQUAL(*number, -2147483648);
 }
 
@@ -145,33 +103,15 @@ BOOST_AUTO_TEST_CASE(kafka_primitives_string_test) {
     kafka::kafka_string_t string("321");
     BOOST_REQUIRE_EQUAL(*string, "321");
 
-    std::array<char, 7> data{0, 5, 'a', 'b', 'c', 'd', 'e'};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(data.begin(), data.end());
-
-    string.deserialize(input_stream, 0);
+    test_deserialize_serialize({0, 5, 'a', 'b', 'c', 'd', 'e'}, string, 0);
     BOOST_REQUIRE_EQUAL(*string, "abcde");
     BOOST_REQUIRE_EQUAL(string->size(), 5);
 
-    std::array<char, 7> output{};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream(output.begin(), output.end());
-    string.serialize(output_stream, 0);
-    BOOST_REQUIRE(!output_stream.bad());
-
-    BOOST_TEST(output == data, boost::test_tools::per_element());
-
-    std::array<char, 5> too_short_data{0, 4, 'a', 'b', 'c'};
-    boost::iostreams::stream<boost::iostreams::array_source> short_input_stream(too_short_data.begin(),
-                                                                                too_short_data.end());
-
-    BOOST_REQUIRE_THROW(string.deserialize(short_input_stream, 0), kafka::parsing_exception);
+    test_deserialize_throw({0, 4, 'a', 'b', 'c'}, string, 0);
     BOOST_REQUIRE_EQUAL(*string, "abcde");
     BOOST_REQUIRE_EQUAL(string->size(), 5);
 
-    std::array<char, 1> too_short_data2{0};
-    boost::iostreams::stream<boost::iostreams::array_source> short_input_stream2(too_short_data2.begin(),
-                                                                                 too_short_data2.end());
-
-    BOOST_REQUIRE_THROW(string.deserialize(short_input_stream2, 0), kafka::parsing_exception);
+    test_deserialize_throw({0}, string, 0);
 }
 
 BOOST_AUTO_TEST_CASE(kafka_primitives_nullable_string_test) {
@@ -179,105 +119,60 @@ BOOST_AUTO_TEST_CASE(kafka_primitives_nullable_string_test) {
     BOOST_REQUIRE(string.is_null());
     BOOST_REQUIRE_THROW((void) *string, std::exception);
 
-    std::array<char, 7> data{0, 5, 'a', 'b', 'c', 'd', 'e'};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(data.begin(), data.end());
-
-    string.deserialize(input_stream, 0);
-    BOOST_REQUIRE(!string.is_null());
+    test_deserialize_serialize({0, 5, 'a', 'b', 'c', 'd', 'e'}, string, 0);
     BOOST_REQUIRE_EQUAL(*string, "abcde");
     BOOST_REQUIRE_EQUAL(string->size(), 5);
 
-    std::array<char, 7> output{};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream(output.begin(), output.end());
-    string.serialize(output_stream, 0);
-    BOOST_REQUIRE(!output_stream.bad());
-
-    BOOST_TEST(output == data, boost::test_tools::per_element());
-
-    std::array<char, 2> null_data{-1, -1};
-    boost::iostreams::stream<boost::iostreams::array_source> null_input_stream(null_data.begin(), null_data.end());
-    string.deserialize(null_input_stream, 0);
-
+    test_deserialize_serialize({0xFF, 0xFF}, string, 0);
     BOOST_REQUIRE(string.is_null());
-
-    std::array<char, 2> null_output{};
-    boost::iostreams::stream<boost::iostreams::array_sink> null_output_stream(null_output.begin(), null_output.end());
-    string.serialize(null_output_stream, 0);
-    BOOST_REQUIRE(!null_output_stream.bad());
-
-    BOOST_TEST(null_output == null_data, boost::test_tools::per_element());
 }
 
 BOOST_AUTO_TEST_CASE(kafka_primitives_bytes_test) {
     kafka::kafka_bytes_t bytes;
 
-    std::array<char, 9> data{0, 0, 0, 5, 'a', 'b', 'c', 'd', 'e'};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(data.begin(), data.end());
-
-    bytes.deserialize(input_stream, 0);
+    test_deserialize_serialize({0, 0, 0, 5, 'a', 'b', 'c', 'd', 'e'}, bytes, 0);
     BOOST_REQUIRE_EQUAL(*bytes, "abcde");
     BOOST_REQUIRE_EQUAL(bytes->size(), 5);
-
-    std::array<char, 9> output{};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream(output.begin(), output.end());
-    bytes.serialize(output_stream, 0);
-
-    BOOST_REQUIRE(!output_stream.bad());
-    BOOST_TEST(output == data, boost::test_tools::per_element());
 }
 
 BOOST_AUTO_TEST_CASE(kafka_primitives_array_test) {
     kafka::kafka_array_t<kafka::kafka_string_t> strings;
 
-    std::array<char, 15> data{0, 0, 0, 2, 0, 5, 'a', 'b', 'c', 'd', 'e', 0, 2, 'f', 'g'};
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(data.begin(), data.end());
+    test_deserialize_serialize({0, 0, 0, 2, 0, 5, 'a', 'b', 'c', 'd', 'e', 0, 2, 'f', 'g'}, strings, 0);
 
-    strings.deserialize(input_stream, 0);
     BOOST_REQUIRE_EQUAL(strings->size(), 2);
     BOOST_REQUIRE_EQUAL(*strings[0], "abcde");
     BOOST_REQUIRE_EQUAL(*strings[1], "fg");
 
-    std::array<char, 15> output{};
-    boost::iostreams::stream<boost::iostreams::array_sink> output_stream(output.begin(), output.end());
-    strings.serialize(output_stream, 0);
-
-    BOOST_REQUIRE(!output_stream.bad());
-    BOOST_TEST(output == data, boost::test_tools::per_element());
-
-    std::array<char, 14> too_short_data{0, 0, 0, 2, 0, 5, 'A', 'B', 'C', 'D', 'E', 0, 2, 'F'};
-    boost::iostreams::stream<boost::iostreams::array_source> short_input_stream(too_short_data.begin(),
-                                                                                too_short_data.end());
-
-    BOOST_REQUIRE_THROW(strings.deserialize(short_input_stream, 0), kafka::parsing_exception);
+    test_deserialize_throw({0, 0, 0, 2, 0, 5, 'A', 'B', 'C', 'D', 'E', 0, 2, 'F'}, strings, 0);
     BOOST_REQUIRE_EQUAL(strings->size(), 2);
     BOOST_REQUIRE_EQUAL(*strings[0], "abcde");
     BOOST_REQUIRE_EQUAL(*strings[1], "fg");
 }
 
 BOOST_AUTO_TEST_CASE(kafka_api_versions_reponse_deserialize_test) {
-    std::array<char, 280> payload{
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x2d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x01, 0x00, 0x00,
-            0x00, 0x0b, 0x00, 0x02, 0x00, 0x00, 0x00, 0x05, 0x00, 0x03, 0x00, 0x00, 0x00, 0x08, 0x00, 0x04,
-            0x00, 0x00, 0x00, 0x02, 0x00, 0x05, 0x00, 0x00, 0x00, 0x01, 0x00, 0x06, 0x00, 0x00, 0x00, 0x05,
-            0x00, 0x07, 0x00, 0x00, 0x00, 0x02, 0x00, 0x08, 0x00, 0x00, 0x00, 0x07, 0x00, 0x09, 0x00, 0x00,
-            0x00, 0x05, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x02, 0x00, 0x0b, 0x00, 0x00, 0x00, 0x05, 0x00, 0x0c,
-            0x00, 0x00, 0x00, 0x03, 0x00, 0x0d, 0x00, 0x00, 0x00, 0x02, 0x00, 0x0e, 0x00, 0x00, 0x00, 0x03,
-            0x00, 0x0f, 0x00, 0x00, 0x00, 0x03, 0x00, 0x10, 0x00, 0x00, 0x00, 0x02, 0x00, 0x11, 0x00, 0x00,
-            0x00, 0x01, 0x00, 0x12, 0x00, 0x00, 0x00, 0x02, 0x00, 0x13, 0x00, 0x00, 0x00, 0x03, 0x00, 0x14,
-            0x00, 0x00, 0x00, 0x03, 0x00, 0x15, 0x00, 0x00, 0x00, 0x01, 0x00, 0x16, 0x00, 0x00, 0x00, 0x01,
-            0x00, 0x17, 0x00, 0x00, 0x00, 0x03, 0x00, 0x18, 0x00, 0x00, 0x00, 0x01, 0x00, 0x19, 0x00, 0x00,
-            0x00, 0x01, 0x00, 0x1a, 0x00, 0x00, 0x00, 0x01, 0x00, 0x1b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1c,
-            0x00, 0x00, 0x00, 0x02, 0x00, 0x1d, 0x00, 0x00, 0x00, 0x01, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x01,
-            0x00, 0x1f, 0x00, 0x00, 0x00, 0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0x02, 0x00, 0x21, 0x00, 0x00,
-            0x00, 0x01, 0x00, 0x22, 0x00, 0x00, 0x00, 0x01, 0x00, 0x23, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24,
-            0x00, 0x00, 0x00, 0x01, 0x00, 0x25, 0x00, 0x00, 0x00, 0x01, 0x00, 0x26, 0x00, 0x00, 0x00, 0x01,
-            0x00, 0x27, 0x00, 0x00, 0x00, 0x01, 0x00, 0x28, 0x00, 0x00, 0x00, 0x01, 0x00, 0x29, 0x00, 0x00,
-            0x00, 0x01, 0x00, 0x2a, 0x00, 0x00, 0x00, 0x01, 0x00, 0x2b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2c,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-    };
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(payload.begin(), payload.end());
     kafka::api_versions_response response;
-    response.deserialize(input_stream, 2);
+    test_deserialize_serialize({
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x2d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x01, 0x00, 0x00,
+        0x00, 0x0b, 0x00, 0x02, 0x00, 0x00, 0x00, 0x05, 0x00, 0x03, 0x00, 0x00, 0x00, 0x08, 0x00, 0x04,
+        0x00, 0x00, 0x00, 0x02, 0x00, 0x05, 0x00, 0x00, 0x00, 0x01, 0x00, 0x06, 0x00, 0x00, 0x00, 0x05,
+        0x00, 0x07, 0x00, 0x00, 0x00, 0x02, 0x00, 0x08, 0x00, 0x00, 0x00, 0x07, 0x00, 0x09, 0x00, 0x00,
+        0x00, 0x05, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x02, 0x00, 0x0b, 0x00, 0x00, 0x00, 0x05, 0x00, 0x0c,
+        0x00, 0x00, 0x00, 0x03, 0x00, 0x0d, 0x00, 0x00, 0x00, 0x02, 0x00, 0x0e, 0x00, 0x00, 0x00, 0x03,
+        0x00, 0x0f, 0x00, 0x00, 0x00, 0x03, 0x00, 0x10, 0x00, 0x00, 0x00, 0x02, 0x00, 0x11, 0x00, 0x00,
+        0x00, 0x01, 0x00, 0x12, 0x00, 0x00, 0x00, 0x02, 0x00, 0x13, 0x00, 0x00, 0x00, 0x03, 0x00, 0x14,
+        0x00, 0x00, 0x00, 0x03, 0x00, 0x15, 0x00, 0x00, 0x00, 0x01, 0x00, 0x16, 0x00, 0x00, 0x00, 0x01,
+        0x00, 0x17, 0x00, 0x00, 0x00, 0x03, 0x00, 0x18, 0x00, 0x00, 0x00, 0x01, 0x00, 0x19, 0x00, 0x00,
+        0x00, 0x01, 0x00, 0x1a, 0x00, 0x00, 0x00, 0x01, 0x00, 0x1b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1c,
+        0x00, 0x00, 0x00, 0x02, 0x00, 0x1d, 0x00, 0x00, 0x00, 0x01, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x01,
+        0x00, 0x1f, 0x00, 0x00, 0x00, 0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0x02, 0x00, 0x21, 0x00, 0x00,
+        0x00, 0x01, 0x00, 0x22, 0x00, 0x00, 0x00, 0x01, 0x00, 0x23, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24,
+        0x00, 0x00, 0x00, 0x01, 0x00, 0x25, 0x00, 0x00, 0x00, 0x01, 0x00, 0x26, 0x00, 0x00, 0x00, 0x01,
+        0x00, 0x27, 0x00, 0x00, 0x00, 0x01, 0x00, 0x28, 0x00, 0x00, 0x00, 0x01, 0x00, 0x29, 0x00, 0x00,
+        0x00, 0x01, 0x00, 0x2a, 0x00, 0x00, 0x00, 0x01, 0x00, 0x2b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2c,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    }, response, 2);
+
     BOOST_REQUIRE_EQUAL(*response.get_throttle_time_ms(), 0);
     BOOST_REQUIRE_EQUAL(*response.get_error_code(), 0);
     BOOST_REQUIRE_EQUAL(response.get_api_keys()->size(), 45);
@@ -290,12 +185,10 @@ BOOST_AUTO_TEST_CASE(kafka_api_versions_reponse_deserialize_test) {
 }
 
 BOOST_AUTO_TEST_CASE(kafka_metadata_request_deserialize_test) {
-    std::array<char, 14> payload{
-            0x00, 0x00, 0x00, 0x01, 0x00, 0x05, 0x74, 0x65, 0x73, 0x74, 0x35, 0x01, 0x00, 0x00
-    };
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(payload.begin(), payload.end());
     kafka::metadata_request request;
-    request.deserialize(input_stream, 8);
+    test_deserialize_serialize({
+        0x00, 0x00, 0x00, 0x01, 0x00, 0x05, 0x74, 0x65, 0x73, 0x74, 0x35, 0x01, 0x00, 0x00
+    }, request, 8);
 
     BOOST_REQUIRE_EQUAL(request.get_topics()->size(), 1);
     BOOST_REQUIRE_EQUAL(*request.get_topics()[0].get_name(), "test5");
@@ -305,20 +198,18 @@ BOOST_AUTO_TEST_CASE(kafka_metadata_request_deserialize_test) {
 }
 
 BOOST_AUTO_TEST_CASE(kafka_metadata_response_deserialize_test) {
-    std::array<unsigned char, 118> payload{
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x0a, 0x31, 0x37,
-            0x32, 0x2e, 0x31, 0x33, 0x2e, 0x30, 0x2e, 0x31, 0x00, 0x00, 0x23, 0x84, 0xff, 0xff, 0x00, 0x16,
-            0x6b, 0x4c, 0x5a, 0x35, 0x6a, 0x50, 0x76, 0x44, 0x52, 0x30, 0x43, 0x77, 0x31, 0x79, 0x34, 0x31,
-            0x41, 0x66, 0x35, 0x48, 0x55, 0x67, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
-            0x00, 0x05, 0x74, 0x65, 0x73, 0x74, 0x35, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
-            0x03, 0xe9, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-    };
-    boost::iostreams::stream<boost::iostreams::array_source> input_stream(reinterpret_cast<char *>(payload.data()),
-                                                                          payload.size());
     kafka::metadata_response response;
-    response.deserialize(input_stream, 8);
+    test_deserialize_serialize({
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x0a, 0x31, 0x37,
+        0x32, 0x2e, 0x31, 0x33, 0x2e, 0x30, 0x2e, 0x31, 0x00, 0x00, 0x23, 0x84, 0xff, 0xff, 0x00, 0x16,
+        0x6b, 0x4c, 0x5a, 0x35, 0x6a, 0x50, 0x76, 0x44, 0x52, 0x30, 0x43, 0x77, 0x31, 0x79, 0x34, 0x31,
+        0x41, 0x66, 0x35, 0x48, 0x55, 0x67, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+        0x00, 0x05, 0x74, 0x65, 0x73, 0x74, 0x35, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+        0x03, 0xe9, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x03, 0xe9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    }, response, 8);
+
     BOOST_REQUIRE_EQUAL(*response.get_throttle_time_ms(), 0);
     BOOST_REQUIRE_EQUAL(response.get_brokers()->size(), 1);
     BOOST_REQUIRE_EQUAL(*response.get_brokers()[0].get_node_id(), 0x3e9);
